d83243079a347ae1ca0498f3fc6f0a392ece587b
Vuong Vu <vu@bikeleasing.de>
2024-07-21 23:38:56 +0200

    chore: update tsconfig to include only the 'lib' directory

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,7 +1,13 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es", "cjs"],
+    },
+  },
 });
