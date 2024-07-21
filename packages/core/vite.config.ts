2e72bea9a7b9507ab4242ca4ec3e138e325eb3e2
Vuong Vu <vu@bikeleasing.de>
2024-07-21 23:47:57 +0200

    chore: Update Vite configuration to include dts plugin

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,9 +1,10 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib"] })],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
